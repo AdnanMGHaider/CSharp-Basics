ec113602263db099d086ac172e7248fa9a7396d8
AHaider <golamhai@sheridancollege.ca>
2025-01-18 18:31:41 -0500

    If-else statements, Switch statements, for, while and do-while loops

--- CSharp-Basics/Program.cs
+++ CSharp-Basics/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace CSharp_Basics
+﻿using System.Collections.Concurrent;
+
+namespace CSharp_Basics
 {
     internal class Program
     {
@@ -209,16 +211,224 @@
             //Console.WriteLine(remainder);
 
             /////Console Input/Output/////
+            //Console.WriteLine("Enter your name: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Your name is " + name);
+
+            ////Console.Write() Does not push everything after to the next line.
+            //Console.Write("Enter your age: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Your age is " + age);
+
+            //Console.WriteLine("Your name is " + name + " and your age is " + age);
+
+            /////If Statements/////
+
             Console.WriteLine("Enter your name: ");
             string name = Console.ReadLine();
-            Console.WriteLine("Your name is " + name);
 
-            //Console.Write() Does not push everything after to the next line.
-            Console.Write("Enter your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your age is " + age);
+            Console.WriteLine("Enter your age: ");
+            string ageInput = Console.ReadLine();
+            int age = Convert.ToInt32(ageInput);
 
             Console.WriteLine("Your name is " + name + " and your age is " + age);
+
+            //==, >, >=, <, <=, !=, ||, &&
+
+            
+              if (age >= 18 && age <= 25)
+            {
+                Console.WriteLine("You are between 18 and 25");
+            }
+            else if (age >= 26)
+            {
+                Console.WriteLine("You are 26 or older");
+            }
+
+
+
+            if (age < 0 || age > 150)
+            {
+                Console.WriteLine("Invalid age!");
+            }
+            else
+            {
+                if (age >= 18 && age <= 25)
+                {
+                    Console.WriteLine("You are between 18 and 25");
+                }
+                else if (age >= 26)
+                {
+                    Console.WriteLine("You are 26 or older");
+                }
+            }
+
+            Console.WriteLine("Enter the first number: ");
+            string numberAInput = Console.ReadLine();
+            int numberA = Convert.ToInt32(numberAInput);
+
+
+            Console.WriteLine("Enter the second number: ");
+            string numberBInput = Console.ReadLine();
+            int numberB = Convert.ToInt32(numberBInput);
+
+            int answer = numberA * numberB;
+
+            Console.WriteLine("Value of " + numberA + " x " + numberB + ": ");
+            string answerInput = Console.ReadLine();
+            int actualAnswer = Convert.ToInt32(answerInput);
+
+            if (answer == actualAnswer)
+            {
+                Console.WriteLine("Well done!");
+            }
+            else
+            {
+                Console.WriteLine("Close but it was wrong!");
+            }
+
+            ///Switch Statements/////
+            Console.WriteLine("Enter a day of the week: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Mon");
+                    break;
+                case 2:
+                    Console.WriteLine("Tue");
+                    break;
+                case 3:
+                    Console.WriteLine("Wed");
+                    break;
+                case 4:
+                    Console.WriteLine("Thu");
+                    break;
+                case 5:
+                    Console.WriteLine("Fri");
+                    break;
+                case 6:
+                    Console.WriteLine("Sat");
+                    break;
+                case 7:
+                    Console.WriteLine("Sun");
+                    break;
+                default:
+                    Console.WriteLine("Invalid, enter a value between 1 and 7");
+                    break;
+            }
+
+            /////For Loop//////
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hi");
+            }
+
+            for (int i = 0; i <= 10; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("What word do you want to repeat");
+            string message = Console.ReadLine();
+
+            Console.WriteLine("And how many times do you want to repeat that word?");
+            int loopCounter = Convert.ToInt32(Console.ReadLine());
+
+            if (loopCounter <= 0)
+            {
+                Console.WriteLine("Sorry, please enter a value above 0");
+            }
+            else
+            {
+                for (int i = 0; i < loopCounter; i++)
+                {
+                    Console.WriteLine(message);
+                }
+            }
+
+            ///While loops/////
+
+            int i = 0;
+
+            while (i < 10)
+            {
+                i++;
+                Console.WriteLine(i);
+            }
+
+            int i = 0;
+
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+
+            Console.WriteLine("Enter the first number: ");
+            string numberAInput = Console.ReadLine();
+            int numberA = Convert.ToInt32(numberAInput);
+
+
+            Console.WriteLine("Enter the second number: ");
+            string numberBInput = Console.ReadLine();
+            int numberB = Convert.ToInt32(numberBInput);
+            Console.WriteLine();
+
+            int answer = numberA * numberB;
+            int actualAnswer = 0;
+
+            Console.WriteLine("What's the value of " + numberA + " x " + numberB + "? ");
+            Console.WriteLine();
+
+            while (answer != actualAnswer)
+            {
+                Console.WriteLine("Enter your answer: ");
+                string answerInput = Console.ReadLine();
+                actualAnswer = Convert.ToInt32(answerInput);
+
+                if (answer != actualAnswer)
+                {
+                    Console.WriteLine("Close but it was wrong!. Try again.");
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("Well done!");
+
+            /////Do-While Loop/////
+            Console.WriteLine("Enter the first number: ");
+            string numberAInput = Console.ReadLine();
+            int numberA = Convert.ToInt32(numberAInput);
+
+
+            Console.WriteLine("Enter the second number: ");
+            string numberBInput = Console.ReadLine();
+            int numberB = Convert.ToInt32(numberBInput);
+            Console.WriteLine();
+
+            int answer = numberA * numberB;
+            int actualAnswer = 0;
+
+            Console.WriteLine("What's the value of " + numberA + " x " + numberB + "? ");
+            Console.WriteLine();
+
+            do
+            {
+                Console.WriteLine("Enter your answer: ");
+                string answerInput = Console.ReadLine();
+                actualAnswer = Convert.ToInt32(answerInput);
+
+                if (answer != actualAnswer)
+                {
+                    Console.WriteLine("Close but it was wrong!. Try again.");
+                    Console.WriteLine();
+                }
+            } while (answer != actualAnswer);
+
+            Console.WriteLine("Well done!");
         }
     }
 }
